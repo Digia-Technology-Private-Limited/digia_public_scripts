4a3b80fc43731b9cc40be96144adf6c04718334a
Paras Lohia <81807209+paras9192@users.noreply.github.com>
2025-02-12 12:52:47 +0530

    changes

--- github/version/1/merge_changes.sh
+++ github/version/1/merge_changes.sh
@@ -16,14 +16,14 @@ echo "Node.js and dependencies installed successfully."
 
 # Fetch the correct version file from S3
 VERSION=$3
-curl -o scripts/yaml_json.js "https://digia-backend-files.s3.ap-south-1.amazonaws.com/github-scripts/version/${VERSION}/yaml_json.js"
+curl -o scripts/yaml_json.js "https://raw.githubusercontent.com/Digia-Technology-Private-Limited/digia_public_scripts/refs/heads/main/github/version/$VERSION/yaml_json.js"
 chmod +x scripts/yaml_json.js
 
 # Run the script with provided arguments
 projectId=$1
 branchName=$2
 
-node scripts/yaml_json.js "$projectId" "$branchname" 
+node scripts/yaml_json.js "$projectId" "$branchName" 
 
 # Cleanup
 chmod -R 777 node_modules package.json package-lock.json scripts/yaml_json.js
